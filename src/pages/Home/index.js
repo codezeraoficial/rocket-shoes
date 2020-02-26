601ab9201c0ca62952cb1dd5e7d025236a89316b
Leonardo Viana <leonardo.viana@meep-app.com>
2020-02-26 10:58:56 -0300

    #5

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import { ProdutList } from './styles';
 import api from '../../services/api';
+
+import * as CartActions from '../../store/modules/cart/actions';
 
 class Home extends Component {
     state = {
@@ -21,17 +24,15 @@ class Home extends Component {
         this.setState({ products: data });
     }
 
-    handleAddProduct = product => {
-        const { dispatch } = this.props;
+    handleAddProduct = id => {
+        const { addToCartRequest } = this.props;
 
-        dispatch({
-            type: 'ADD_TO_CART',
-            product,
-        });
+        addToCartRequest(id);
     };
 
     render() {
         const { products } = this.state;
+        const { amount } = this.props;
         return (
             <ProdutList>
                 {products.map(product => (
@@ -42,10 +43,11 @@ class Home extends Component {
 
                         <button
                             type="button"
-                            onClick={() => this.handleAddProduct(product)}
+                            onClick={() => this.handleAddProduct(product.id)}
                         >
                             <div>
-                                <MdAddShoppingCart size={16} color="#FFF" /> 3
+                                <MdAddShoppingCart size={16} color="#FFF" />{' '}
+                                {amount[product.id] || 0}
                             </div>
 
                             <span>ADICIONAR AO CARRINHO</span>
@@ -57,4 +59,14 @@ class Home extends Component {
     }
 }
 
-export default connect()(Home);
+const mapStateToProps = state => ({
+    amount: state.cart.reduce((amount, product) => {
+        amount[product.id] = product.amount;
+        return amount;
+    }, {}),
+});
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(CartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,73 +1,69 @@
 import styled from 'styled-components';
-import {darken} from 'polished';
+import { darken } from 'polished';
+
 export const ProdutList = styled.ul`
-display: grid;
-grid-template-columns: repeat(3, 1fr);
-grid-gap: 20px;
-list-style: none;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 20px;
+    list-style: none;
 
-li{
-    display: flex;
-    flex-direction: column;
-    background: #FFF;
-    border-radius: 4px;
-    padding: 20px;
-
-img{
-    align-self: center;
-    max-width: 250px;
-}
-
-> strong{
-    font-size: 16px;
-    line-height: 20px;
-    color: #333;
-    marker-start: 5px
-}
-
-> span{
-    font-size: 21px;
-    font-weight: bold;
-    margin: 5px 0 20px;
-}
-
-button{
-    background: #7159c1;
-    color: #FFF;
-    border: 0;
-    border-radius: 4px;
-    overflow: hidden;
-    margin-top: auto;
-
-    display: flex;
-    align-items: center;
-    transition: background 0.2s;
-
-    &:hover{
-     background: ${darken(0.03, '#7159c1')}
-    }
-
-    div{
+    li {
         display: flex;
-        align-items: center;
-        padding: 12px;
-        background: rgba(0, 0, 0, 0.1);
+        flex-direction: column;
+        background: #fff;
+        border-radius: 4px;
+        padding: 20px;
+
+        img {
+            align-self: center;
+            max-width: 250px;
+        }
+
+        > strong {
+            font-size: 16px;
+            line-height: 20px;
+            color: #333;
+            marker-start: 5px;
+        }
+
+        > span {
+            font-size: 21px;
+            font-weight: bold;
+            margin: 5px 0 20px;
+        }
+
+        button {
+            background: #7159c1;
+            color: #fff;
+            border: 0;
+            border-radius: 4px;
+            overflow: hidden;
+            margin-top: auto;
+
+            display: flex;
+            align-items: center;
+            transition: background 0.2s;
+
+            &:hover {
+                background: ${darken(0.03, '#7159c1')};
+            }
+
+            div {
+                display: flex;
+                align-items: center;
+                padding: 12px;
+                background: rgba(0, 0, 0, 0.1);
+            }
+
+            svg {
+                margin-right: 5px;
+            }
+
+            span {
+                flex: 1;
+                text-align: center;
+                font-weight: bold;
+            }
+        }
     }
-
-    svg{
-        margin-right: 5px;
-    }
-
-    span{
-        flex: 1;
-        text-align: center;
-        font-weight: bold;
-    }
-}
-
-}
-
-
-
 `;
-
